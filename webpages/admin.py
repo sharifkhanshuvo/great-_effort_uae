6f6c4766d5bdcf247cd5c49e61ef7c43ea79a010
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2021-06-21 10:48:47 +0600

    changed models

--- webpages/admin.py
+++ webpages/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import HeroSlider
+from .models import HeroSlider,Service,Testimonial
 from django.utils.html import format_html
 # Register your models here.
 
@@ -8,6 +8,9 @@ from django.utils.html import format_html
 
    
 admin.site.register(HeroSlider)
+admin.site.register(Service)
+admin.site.register(Testimonial)
+
 
 
 
--- webpages/models.py
+++ webpages/models.py
@@ -8,6 +8,23 @@ class HeroSlider(models.Model):
     photo = models.ImageField(upload_to='heroSlider/')
 
 
+class Service(models.Model):
+    CATEGORY_CHOICES= (
+    (
+        'mep','mep'
+    ),
+     (
+        'contracting','contracting'
+    ),
+    )
+    title=models.CharField(max_length=50)
+    description=models.CharField(max_length=100)
+    photo=models.ImageField(upload_to='services/')
+    category=models.CharField(max_length=50,choices=CATEGORY_CHOICES)
 
 
-
+class Testimonial(models.Model):
+    personname=models.CharField(max_length=50)
+    personprofession=models.CharField(max_length=50)
+    testimonial=models.CharField(max_length=50)
+    photo=models.ImageField(upload_to='testimonial/')
