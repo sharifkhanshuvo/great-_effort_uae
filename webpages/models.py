0cdb1738cf44dd961541e9592f27fd5dc5f1cbf6
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2021-06-21 11:19:21 +0600

    updated

--- webpages/models.py
+++ webpages/models.py
@@ -7,6 +7,9 @@ class HeroSlider(models.Model):
     subtitle=models.CharField(max_length=50)
     photo = models.ImageField(upload_to='heroSlider/')
 
+    def __str__(self):
+        return self.title
+
 
 class Service(models.Model):
     CATEGORY_CHOICES= (
@@ -22,9 +25,15 @@ class Service(models.Model):
     photo=models.ImageField(upload_to='services/')
     category=models.CharField(max_length=50,choices=CATEGORY_CHOICES)
 
+    def __str__(self):
+        return self.title
+
 
 class Testimonial(models.Model):
     personname=models.CharField(max_length=50)
     personprofession=models.CharField(max_length=50)
     testimonial=models.CharField(max_length=50)
     photo=models.ImageField(upload_to='testimonial/')
+
+    def __str__(self):
+        return self.personname
