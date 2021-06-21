ec4ca209e67c85a049b163ff306d4464b46ad137
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2021-06-21 16:50:58 +0600

    added project,about,team section in admin panel

--- webpages/admin.py
+++ webpages/admin.py
@@ -1,15 +1,29 @@
 from django.contrib import admin
-from .models import HeroSlider,Service,Testimonial
+from .models import HeroSlider,Service,Testimonial,About,Team,Project
 from django.utils.html import format_html
 # Register your models here.
 
+class AboutAdmin(admin.ModelAdmin):
 
+
+    def has_add_permission(self, request):
+
+    # if there's already an entry, do not allow adding
+        count = HeroSlider.objects.all().count()
+        if count == 0:
+
+            return True
+
+        return False
 
 
    
 admin.site.register(HeroSlider)
 admin.site.register(Service)
 admin.site.register(Testimonial)
+admin.site.register(About,AboutAdmin)
+admin.site.register(Team)
+admin.site.register(Project)
 
 
 
--- webpages/models.py
+++ webpages/models.py
@@ -11,6 +11,42 @@ class HeroSlider(models.Model):
         return self.title
 
 
+class About(models.Model):
+    title=models.CharField(max_length=50)
+    description=models.CharField(max_length=300)
+    photo= models.ImageField(upload_to='aboutus/')
+
+    def __str__(self):
+        return self.title
+
+
+class Team(models.Model):
+    TEAM_CHOICES= (
+    (
+        'administrator','administrator'
+    ),
+     (
+        'engineering','engineering'
+    ),
+    )
+    name=models.CharField(max_length=50)
+    post=models.CharField(max_length=50)
+    photo=models.ImageField(upload_to='teamimages/')
+    teamtype=models.CharField(max_length=50,choices=TEAM_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.CharField(max_length=300)
+    photo = models.ImageField(upload_to='projects/')
+    def __str__(self):
+        return self.name
+
+
+
 class Service(models.Model):
     CATEGORY_CHOICES= (
     (
