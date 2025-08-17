fa80e4e2a60a5236e2f966473535881edca87d03
sharif uddin khan <35971432+sharifkhanshuvo@users.noreply.github.com>
2025-08-17 16:22:42 +0600

    Update admin.py

--- webpages/admin.py
+++ webpages/admin.py
@@ -1,86 +1,217 @@
 from django.contrib import admin
-from .models import (
-    HeroSlider,
-    Service,
-    Testimonial,
-    About,
-    Team,
-    Project,
-    Blog,
-    Client,
-    ExpertWorker,
-    QualityWork,
-    Support,
-    AboutMore,
-    ContactForm,
-    ExpertWorkerCount,
-    ClientsCount,
-    CompletedProjectCount,
-    RunningProjectCount,
-)
+from .models import HeroSlider,Service,Testimonial,About,Team,Project,Blog,Client,ExpertWorker,QualityWork,Support,AboutMore,ContactForm,ExpertWorkerCount,ClientsCount,CompletedProjectCount,RunningProjectCount
+from django.utils.html import format_html
+# Register your models here.
 
-# A reusable admin that allows only a single row and disables delete
-class SingleInstanceAdmin(admin.ModelAdmin):
+
+class AboutAdmin(admin.ModelAdmin):
+
     def has_add_permission(self, request):
-        # Allow "Add" only if no instance exists
-        return not self.model.objects.exists()
 
-    def has_delete_permission(self, request, obj=None):
-        # Never allow delete from admin
+
+    # if there's already an entry, do not allow adding
+        count = About.objects.all().count()
+        if count == 0:
+
+
+            return True
+
+        return False
+
+    def has_delete_permission(self, request,obj=None):
+
         return False
 
 
-@admin.register(About)
-class AboutAdmin(SingleInstanceAdmin):
-    pass
+
+class AboutMoreAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
 
 
-@admin.register(AboutMore)
-class AboutMoreAdmin(SingleInstanceAdmin):
-    pass
+
+    # if there's already an entry, do not allow adding
+        count = AboutMore.objects.all().count()
+        if count == 0:
 
 
-@admin.register(ExpertWorker)
-class ExpertWorkerAdmin(SingleInstanceAdmin):
-    pass
+
+            return True
+
+        return False
+
+    def has_delete_permission(self, request,obj=None):
+        
+
+        return False
 
 
-@admin.register(QualityWork)
-class QualityWorkAdmin(SingleInstanceAdmin):
-    pass
+class ExpertWorkerAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
 
 
-@admin.register(Support)
-class SupportAdmin(SingleInstanceAdmin):
-    pass
+
+    # if there's already an entry, do not allow adding
+        count = ExpertWorker.objects.all().count()
+        if count == 0:
 
 
-@admin.register(ExpertWorkerCount)
-class ExpertWorkerCountAdmin(SingleInstanceAdmin):
-    pass
+
+            return True
+
+        return False
+
+    def has_delete_permission(self, request,obj=None):
+
+        return False
+
+class QualityWorkAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
 
 
-@admin.register(ClientsCount)
-class ClientsCountAdmin(SingleInstanceAdmin):
-    pass
+
+    # if there's already an entry, do not allow adding
+        count = QualityWork.objects.all().count()
+        if count == 0:
 
 
-@admin.register(CompletedProjectCount)
-class CompletedProjectCountAdmin(SingleInstanceAdmin):
-    pass
+
+            return True
+
+        return False
+
+    def has_delete_permission(self, request,obj=None):
+
+        return False
 
 
-@admin.register(RunningProjectCount)
-class RunningProjectCountAdmin(SingleInstanceAdmin):
-    pass
+
+class SupportAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
 
 
-# Regular models (no singleton logic)
+
+    # if there's already an entry, do not allow adding
+        count = Support.objects.all().count()
+        if count == 0:
+
+
+
+            return True
+
+        return False
+
+    def has_delete_permission(self, request,obj=None):
+
+        return False
+
+
+class ExpertWorkerCountAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+
+
+
+    # if there's already an entry, do not allow adding
+        count = ExpertWorkerCount.objects.all().count()
+        if count == 0:
+
+
+
+            return True
+
+        return False
+
+    def has_delete_permission(self, request,obj=None):
+
+        return False
+
+class ClientsCountAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+
+
+
+    # if there's already an entry, do not allow adding
+        count = ClientsCount.objects.all().count()
+        if count == 0:
+
+
+
+            return True
+
+        return False
+
+    def has_delete_permission(self, request,obj=None):
+
+        return False
+
+
+class CompletedProjectCountAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+
+
+
+    # if there's already an entry, do not allow adding
+        count = CompletedProjectCount.objects.all().count()
+        if count == 0:
+
+
+
+            return True
+
+        return False
+
+    def has_delete_permission(self, request,obj=None):
+
+        return False
+
+class RunningProjectCountAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+
+
+
+    # if there's already an entry, do not allow adding
+        count = RunningProjectCount.objects.all().count()
+        if count == 0:
+
+
+
+            return True
+
+        return False
+
+    def has_delete_permission(self, request,obj=None):
+
+        return False
+
+
+
 admin.site.register(HeroSlider)
 admin.site.register(Service)
 admin.site.register(Testimonial)
+admin.site.register(About,AboutAdmin)
 admin.site.register(Team)
 admin.site.register(Project)
 admin.site.register(Blog)
 admin.site.register(Client)
+admin.site.register(ExpertWorker,ExpertWorkerAdmin)
+admin.site.register(QualityWork,QualityWorkAdmin)
+admin.site.register(Support,SupportAdmin)
+admin.site.register(AboutMore,AboutMoreAdmin)
 admin.site.register(ContactForm)
+admin.site.register(ExpertWorkerCount,ExpertWorkerCountAdmin)
+admin.site.register(ClientsCount,ClientsCountAdmin)
+admin.site.register(CompletedProjectCount,CompletedProjectCountAdmin)
+admin.site.register(RunningProjectCount,RunningProjectCountAdmin)
+
+
+
+
+
+
